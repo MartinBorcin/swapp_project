eca760e1455b6ff874db5d48de66921b71fb7a71
Donmaxo <maximilian.donar@gmail.com>
2021-03-18 12:22:11 +0100

    Models 1st commit

--- swapp/admin.py
+++ swapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'seller',  'price', 'sold', 'checked')
+
 
 # Register your models here.
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Event)
+admin.site.register(Checkout)
+admin.site.register(Announcement)
